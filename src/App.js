0fdfee5d6f23b0061fbf7022f422103616f82f87
Sakif Abdullah <sakif@Sakifs-MacBook-Air.local>
2022-06-04 21:51:08 +0600

    simple counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1> 
+      <button onClick={() => setCounter((Count) => Count + 1)}>+</button>
+      <button onClick={() => setCounter(0)}>Clear</button>
+      <button onClick={() => setCounter((Count) => Count - 1)}>-</button>
     </div>
   );
 }
